460da5934ef8e85f05129fbc60e33e6f63764e7c
tuannt <trongtuan1103@gmail.com>
2024-05-08 07:28:06 +0700

    update

--- src/main/java/com/member/management/controller/AuthenticationController.java
+++ src/main/java/com/member/management/controller/AuthenticationController.java
@@ -1,18 +1,19 @@
 package com.member.management.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
+@Controller
+@RequestMapping()
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     @GetMapping("/login")
-    public String login() {
+    public String showLogin() {
         return "/login";
     }
+
 
 }
---
+++ src/main/java/com/member/management/util/PasswordTest.java
@@ -0,0 +1,9 @@
+package com.member.management.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordTest {
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("user"));
+    }
+}
